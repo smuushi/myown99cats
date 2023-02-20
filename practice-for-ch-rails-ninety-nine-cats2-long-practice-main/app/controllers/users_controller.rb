ac027ce013e9fd15aac19a3f83149e4de629de7e
smuushi <mishih780@gmail.com>
2023-02-20 14:59:41 -0500

    hi steve

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersControler < ApplicationController
+
+    
+
+end
--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
     validates :password_digest, presence: true
     validates :password, length: {minimum: 4}, allow_nil: true
 
+    before_validation :ensure_session_token
+
     attr_reader :password
 
     def password=(password)
@@ -26,6 +28,48 @@ class User < ApplicationRecord
         b_obj = BCrypt::Password.new(self.password_digest)
         b_obj.is_password?(password)
     end
+
+    def self.find_by_credentials(username, password)
+
+        @user = User.find_by(username: username)
+
+        if @user && @user.is_passwordd?(password)
+            return @user
+        else
+            return nil  
+        end
+
+    end
+
+
+    def reset_session_token!
+
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+
+    end
+
+
+    private
+
+    def generate_unique_session_token 
+
+        token = SecureRandom.urlsafe_base64
+
+        while User.exists?(session_token: token)
+            token = SecureRandom.urlsafe_base64
+        end
+
+        return token
+    end
+
+    def ensure_session_token
+
+        self.session_token ||= generate_unique_session_token
+
+    end
+
 
     
 end
