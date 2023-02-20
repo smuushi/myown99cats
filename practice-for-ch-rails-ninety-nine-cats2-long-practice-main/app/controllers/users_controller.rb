2a113ce4cb40c6a51f24fcf56deebf8e9bb707dc
Steven Paalz <stevenpaalz@gmail.com>
2023-02-20 15:16:31 -0500

    hi michael

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
-class UsersControler < ApplicationController
+class UsersController < ApplicationController
 
-    
+    def new
+        @user = User.new
+        render :new
+    end
 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to cats_url
+        else
+            redirect_to new_user_url
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user_info).permit(:username, :password)
+    end
 end
