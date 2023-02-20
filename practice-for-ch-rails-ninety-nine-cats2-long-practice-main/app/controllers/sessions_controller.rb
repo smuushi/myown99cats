c0b608f4e8fb63eaaeaa2af3b19d8d92fb2ace9b
smuushi <mishih780@gmail.com>
2023-02-20 15:46:45 -0500

    doing well

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
@@ -1,5 +1,30 @@
 class SessionsController < ApplicationController
 
+    def new
+        render :new
+    end
+
+
+    def create
+        @user = User.find_by_credentials(params[:user_info][:username], params[:user_info][:password])
+        
+        # debugger
+        session[:session_token] = @user.reset_session_token!
+
+        redirect_to cats_url
+
+
+    end
+
+
+    def destroy
+        if current_user
+            current_user.reset_session_token!
+            session[:session_token] = nil
+        end
+    end
+
     
+
 
 end
